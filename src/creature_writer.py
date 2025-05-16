4daa8b1373ec01b5081380af5e30389643fc40ea
Epsilon <epsijes@gmail.com>
2025-05-16 09:28:08 +0200

    Update creature_writer.py
    Generate md document and properties

--- src/creature_writer.py
+++ src/creature_writer.py
@@ -7,3 +7,19 @@ class Creature_Writer(Writer):
         super().__init__(path, source, license)
         self.description = creature
     
+    def write_properties(self):
+        with open(self.path, 'w') as prop_path:
+            prop_path.write("---\n")
+            prop_path.write(f"title: \"{self.description.get("title")}\"\n")
+            prop_path.write(f"obsidianUIMode: preview\n")
+            prop_path.write(f"notetype: pf2eMonster\n")
+            prop_path.write(f"cssClasses:\n  - pf2e\n")
+            prop_path.write(f"tags:\n")
+            for tag in self.description.get("tags"):
+                prop_path.write(f"  - {tag}\n")
+            prop_path.write(f"statblock: inline\n")
+            prop_path.write(f"name: \"{self.description.get("name")}\"\n")
+            prop_path.write(f"level: {self.description.get("level")}\n")
+            prop_path.write(f"license: {self.license}\n")
+            prop_path.write("---\n\n")
+
